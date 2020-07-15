51664708180952dfcf95d285ef6cfe7de4ec4164
Maai Ham <mzmaai70@gmail.com>
2020-07-15 13:25:50 -0600

    folders and Names
    Reorder folders and proyect namespace conflict by a "-"

---
+++ AuctionCenter.BackEnd/AuctionCenter.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuctionCenter.CORE.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AuctionCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private IConfiguration _config;
+
+        public AuthenticationController(IConfiguration config)
+        {
+            _config = config;
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody]UserRequestInfo login)
+        {
+            IActionResult response = Unauthorized();
+
+            return response;
+        }
+
+
+    
+
+    }
+}
---
+++ AuctionCenter.BackEnd/AuctionCenter.CORE/Entities/UserRequestInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuctionCenter.CORE.Entities
+{
+    public class UserRequestInfo
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
