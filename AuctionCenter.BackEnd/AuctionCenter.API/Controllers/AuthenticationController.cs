0ea2ebe22b739337192e74937fd92453ba6eb466
Maai Ham <mzmaai70@gmail.com>
2020-07-15 13:48:18 -0600

    added auth (backend)
    added JWT auth on api

--- AuctionCenter.BackEnd/AuctionCenter.API/Controllers/AuthenticationController.cs
+++ AuctionCenter.BackEnd/AuctionCenter.API/Controllers/AuthenticationController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using AuctionCenter.CORE.Entities;
@@ -28,12 +29,37 @@ namespace AuctionCenter.API.Controllers
         public IActionResult Login([FromBody]UserRequestInfo login)
         {
             IActionResult response = Unauthorized();
-
+            String tokenString = GenerateJWT(login);
+            response = Ok(new { token = tokenString });
             return response;
         }
 
+        private string GenerateJWT(UserRequestInfo userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var header = new JwtHeader(credentials);
+            var claims = new[] {
+            new Claim("userName",userInfo.Username),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+            var expiration = _config["JWT:ExpirationMins"];
+            var payload = new JwtPayload(
+                   issuer: _config["JWT:Issuer"],
+                   audience: _config["JWT:Audience"],
+                   claims: claims,
+                   notBefore: DateTime.UtcNow,
+                   expires: DateTime.UtcNow.AddMinutes(int.Parse(expiration))
+               );
 
-    
+            var token = new JwtSecurityToken(
+                    header,
+                    payload
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
 
     }
 }
